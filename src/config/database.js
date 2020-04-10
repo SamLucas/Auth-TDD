81c558846a6440650175782ff3c5c3a307aa2dde
Samuel Lucas <samuellucas0603@gmail.com>
2020-04-10 11:39:37 -0300

    lib: dotenv e sqlite.

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,24 @@
-module.exports = {
-  host: "localhost",
-  username: "root",
-  password: "eunaoseiminhasenha",
-  database: "nodeauth",
-  dialect: "postgres",
-  operatorsAliases: false,
-  logging: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true
-  }
+require("dotenv/config");
+
+const ConfigEnvironment = {
+  development: {
+    dialect: "sqlite",
+    storage: "../database/db.sqlite",
+  },
+  production: {
+    host: "localhost",
+    username: "root",
+    password: "eunaoseiminhasenha",
+    database: "nodeauth",
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
 };
+
+module.exports = ConfigEnvironment[process.env.ENVIRONMENT];
